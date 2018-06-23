e2ca444546e2e3120ea48aefd6c855a65b789791
successor_of_communist <13277996909@163.com>
2018-06-23 21:10:06 +0800

    add

---
+++ ACM/hdu/3836.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+const int MAXN = 20010;//点数
+const int MAXM = 100010;//边数
+struct Edge
+{
+    int to,next;
+}edge[MAXM];
+int head[MAXN],tot;
+int Low[MAXN],DFN[MAXN],Stack[MAXN],Belong[MAXN];//Belong数组的值是1~scc
+int Index,top;
+int scc;//强连通分量的个数
+bool Instack[MAXN];
+int indeg[MAXN],outdeg[MAXN];
+void addedge(int u,int v)
+{
+    edge[tot].to = v;edge[tot].next = head[u];head[u] = tot++;
+}
+void Tarjan(int u)
+{
+    int v;
+    Low[u] = DFN[u] = ++Index;
+    Stack[top++] = u;
+    Instack[u] = true;
+    for(int i = head[u];i != -1;i = edge[i].next)
+    {
+        v = edge[i].to;
+        if( !DFN[v] )
+        {
+            Tarjan(v);
+            if( Low[u] > Low[v] )Low[u] = Low[v];
+        }
+        else if(Instack[v] && Low[u] > DFN[v])
+            Low[u] = DFN[v];
+    }
+    if(Low[u] == DFN[u])
+    {
+        scc++;
+        do
+        {
+            v = Stack[--top];
+            Instack[v] = false;
+            Belong[v] = scc;
+        }
+        while( v != u);
+    }
+}
+void solve(int N)
+{
+    memset(DFN,0,sizeof(DFN));
+    memset(Instack,false,sizeof(Instack));
+    memset(indeg,0,sizeof(indeg));
+    memset(outdeg,0,sizeof(outdeg));
+    Index = scc = top = 0;
+    for(int i = 1;i <= N;i++)
+        if(!DFN[i])
+            Tarjan(i);
+    if(scc==1)
+    {
+        printf("0\n");
+        return;
+    }
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=head[i];j!=-1;j=edge[j].next)
+        {
+            int v=edge[j].to;
+            if(Belong[i]!=Belong[v])
+            {
+                indeg[Belong[v]]++;
+                outdeg[Belong[i]]++;
+            }
+        }
+    }
+    int ansin=0,ansout=0;
+    for(int i=1;i<=scc;i++)
+    {
+        if(indeg[i]==0)
+            ansin++;
+        if(outdeg[i]==0)
+            ansout++;
+    }
+    int ans=max(ansin,ansout);
+    printf("%d\n",ans);
+}
+void init()
+{
+    tot = 0;
+    memset(head,-1,sizeof(head));
+}
+int main(){
+    int n,m,u,v;
+    while(~scanf("%d%d",&n,&m))
+    {
+        init();
+        while(m--)
+        {
+            scanf("%d%d",&u,&v);
+            addedge(u,v);
+        }
+        solve(n);
+    }
+    return 0;
+}
+
